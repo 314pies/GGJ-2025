31514cf6967e1aceb0dcbf0529740bdaa4a92ac2
Casey Avila <casey@theavilas.org>
2025-01-25 14:09:44 -0800

    Add update floor script

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -2,25 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Floor : MonoBehaviour
 {
     public GameObject bubblePrefab;
+    public float avgDistance;
+    public float horizontalRange;
+    public float verticalRange;
+    public float scaleRange;
+    public int width;
 
     void Start()
     {
+        for (int i = -width; i < width; i++) {
+            for (int j = -width; j < width; j++) {
+                float xOff = Random.Range(-horizontalRange, horizontalRange);
+                float zOff = Random.Range(-horizontalRange, horizontalRange);
+                float yOff = Random.Range(-verticalRange, verticalRange);
+                float scale = Random.Range(1 - scaleRange, 1 + scaleRange);
 
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < 20; j++) {
-                float xOff = Random.Range(-0.2f, 0.2f);
-                float zOff = Random.Range(-0.2f, 0.2f);
-                float yOff = Random.Range(-0.2f, 0.2f);
-                Instantiate(bubblePrefab, new Vector3(i * 0.8f + xOff, 0 + yOff, j * 0.8f + zOff), Quaternion.identity);
+                GameObject bubble = Instantiate(bubblePrefab,
+                    transform.position + new Vector3(i * avgDistance + xOff, yOff, j * avgDistance + zOff),
+                    Quaternion.identity);
+
+                bubble.transform.localScale *= scale;
             }
         }
-    }
-
-    void Update()
-    {
-        
     }
 }
