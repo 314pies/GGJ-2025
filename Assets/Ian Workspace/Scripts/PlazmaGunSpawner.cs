4d4a94366f685a592ec0fa57fc8c893adc57e859
314pies <ian121363@gmail.com>
2025-01-27 08:40:46 +0800

    Add gun respawn

--- Assets/Ian Workspace/Scripts/PlazmaGunSpawner.cs
+++ Assets/Ian Workspace/Scripts/PlazmaGunSpawner.cs
@@ -9,20 +9,30 @@ public class PlazmaGunSpawner : NetworkBehaviour
 
     public Transform[] spawnPoints;
     public float spawnRadious = 15, spawnHeight = 1.0f;
+
+    IEnumerator spawn()
+    {
+        while(true)
+        {
+            foreach (var sp in spawnPoints)
+            {
+                float xVar = Random.Range(-spawnRadious, spawnRadious);
+                float zVar = Random.Range(-spawnRadious, spawnRadious);
+
+                Vector3 spos = new Vector3(
+                    sp.position.x + xVar,
+                    sp.position.y + spawnHeight,
+                    sp.position.z + zVar
+                    );
+                GameObject instance = Instantiate(plazmaGun, spos, Quaternion.identity);
+                NetworkServer.Spawn(instance);
+            }
+            yield return new WaitForSeconds(60.0f);
+        }
+    }
+
     public override void OnStartServer()
     {
-        foreach (var sp in spawnPoints)
-        {
-            float xVar = Random.Range(-spawnRadious, spawnRadious);
-            float zVar = Random.Range(-spawnRadious, spawnRadious);
-
-            Vector3 spos = new Vector3(
-                sp.position.x + xVar,
-                sp.position.y + spawnHeight,
-                sp.position.z + zVar
-                );
-            GameObject instance = Instantiate(plazmaGun, spos, Quaternion.identity);
-            NetworkServer.Spawn(instance);
-        }
+        StartCoroutine(spawn());
     }
 }
